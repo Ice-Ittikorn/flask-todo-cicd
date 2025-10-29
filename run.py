6601eab79aae1e8609040da76ae810dedcf258db
Ice-Ittikorn <67030261@kmitl.ac.th>
2025-10-29 23:26:31 +0700

    cicd

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
+
+CORS(app, origins=["https://ice-ittikorn.github.io"], supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
